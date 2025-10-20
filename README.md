be160b596b5b82a304922d4f9387710aae4527c4
Patty <patty@pop-os.localdomain>
2025-10-20 16:07:58 -0300

    adiciona scripts `lint:check`e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
-Novos projetos 
+
+Novos projetos
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
- function Home() { 
+function Home() {
+  return <h1>Se você não sabe tocar piano, da uma risadinha!</h1>;
+}
 
-    return <h1>Se você não sabe tocar piano, da uma risadinha!</h1>
- }
-
- export default Home;
+export default Home;
